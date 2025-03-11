5b8cb8c30a53b5c8309aeff24852a699628d3cb8
kicchomen <kicchomen@gmail.com>
2025-03-11 09:59:48 +0900

    Add debounce utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Creates a debounced function that delays invoking the provided function
+ * until after the specified wait time has elapsed since the last time it was invoked.
+ */
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: number | undefined;
+  
+  return function(...args: Parameters<T>) {
+    const later = () => {
+      timeout = undefined;
+      func(...args);
+    };
+    
+    clearTimeout(timeout);
+    timeout = window.setTimeout(later, wait);
+  };
+}
